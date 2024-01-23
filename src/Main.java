27b136f48feac79b7aae54aab988eddc439460d6
Lutti Davide <luttidavide004@gmail.com>
2024-01-23 16:06:54 +0100

    first commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+        Rettangolo rettangolo = new Rettangolo(10, 5);
+        Triangolo triangolo = new Triangolo(10, 5);
+
+        System.out.println("L'area del rettangolo è: " + rettangolo.calcolaArea());
+        System.out.println("L'area del triangolo è: " + triangolo.calcolaArea());
+    }
+}
